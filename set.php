61861abe144dd88820b96cfdf9e70859c4318847
Dubidu1212 <rz.ausserschwand@gmail.com>
2018-08-11 19:35:50 +0200

    improved set.php

--- set.php
+++ set.php
@@ -1,6 +1,28 @@
 <?php
-echo "Gesetzte Tel: ". $_GET['SetTel'];
-echo "Gesetzter Geburtstag: ". $_GET['SetGeb'];
+$Tel = $_GET['SetTel'];
+$BD = $_GET['SetGeb'];
+$servername = "localhost";
+$username = "user";
+$password = "123456";
+$list = "BirthdayFinder";
+$TempArr = array();
+
+$connection = new mysqli($servername, $username, $password,$list);
+if($conection->connect_error){
+  die("Connetion failed: " . $conn->connectt_error);
+
+}
+//check for format
+
+$TempArr = $connection->query("select * from BirthdayFinder WHERE Tel = '$Tel'");
+if(count($TempArr) > 0){
+  header("Location: geburtstagsfinder.html");
+}
+else {
+  $sql = "INSERT INTO BirthdayFinder VALUE ('$Tel','$BD')";
+}
 
 
+
+$connection->close();
 ?>
