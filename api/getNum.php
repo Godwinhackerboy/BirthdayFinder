5e08ff35d63edf406680acbbcad27b69c66517e4
Dubidu1212 <rz.ausserschwand@gmail.com>
2018-08-15 20:38:49 +0200

    included secrets.php in api

--- api/getNum.php
+++ api/getNum.php
@@ -1,14 +1,14 @@
 <?php
-	$link = new mysqulli("localhost", "root","123456","Birthdayfinder");
+	$link = new mysqulli($servername, $username, $password,$list);
 	$QNum = "";
 	$result = $link->query("select * from TelNum WHERE Number = '$QNum'");
 	$json = array();
 	while($row = $result->fetch_assoc()){
 		//var_dump($row);
 		$json[] = $row;
-		
-		
-		
+
+
+
 	}
 	print json_encode($json);
 
